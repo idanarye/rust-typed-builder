fb96cc6f1e49ab430a1af15b7be64745a9d5a33c
treysidechain <trey@sidechainme.com>
2022-12-07 04:47:46 -0800

    add complex default/no-default generic dependent field type test to examples

--- examples/default_generics.rs
+++ examples/default_generics.rs
@@ -15,6 +15,15 @@ struct Foo<T = usize> {
     x: T,
 }
 
+#[allow(dead_code)]
+#[derive(TypedBuilder)]
+struct Bar<T, U = usize, V = usize> {
+    t: T,
+    #[builder(default = 12)]
+    u: U,
+    v: (T, U, V),
+}
+
 fn main() {
     let props = Props::builder().label("label").on_input(|x: usize| x).build();
     assert_eq!(props.class, None);
@@ -22,4 +31,6 @@ fn main() {
     assert_eq!((props.on_input.unwrap())(123), 123);
 
     assert_eq!(Foo::builder().build().x, 12);
+
+    assert_eq!(Bar::builder().t("test").v(("t", 0, 3.14f64)).build().v.0, "t");
 }
