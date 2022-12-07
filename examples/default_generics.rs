4094904377dcc90e4ef84f6e9aafa501a070ae17
treysidechain <trey@sidechainme.com>
2022-12-07 04:16:51 -0800

    add example of working support for issue 44

--- examples/default_generics.rs
+++ examples/default_generics.rs
@@ -9,9 +9,17 @@ pub struct Props<'a, OnInput: FnOnce(usize) -> usize = Box<dyn FnOnce(usize) -> 
     pub on_input: Option<OnInput>,
 }
 
+#[derive(TypedBuilder)]
+struct Foo<T = usize> {
+    #[builder(default = 12)]
+    x: T,
+}
+
 fn main() {
     let props = Props::builder().label("label").on_input(|x: usize| x).build();
     assert_eq!(props.class, None);
     assert_eq!(props.label, "label");
     assert_eq!((props.on_input.unwrap())(123), 123);
+
+    assert_eq!(Foo::builder().build().x, 12);
 }
