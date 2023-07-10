6ec25b9f9458004ac419a17bd9df0e9bd6cf3ff0
Andrea Fioraldi <andreafioraldi@gmail.com>
2023-07-10 13:50:38 +0200

    Add no_std compatibility

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![no_std]
+
+use core::ops::FnOnce;
+
 /// `TypedBuilder` is not a real type - deriving it will generate a `::builder()` method on your
 /// struct that will return a compile-time checked builder. Set the fields using setters with the
 /// same name as the struct's fields and call `.build()` when you are done to create your object.
